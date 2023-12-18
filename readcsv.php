8433d07b461ce00a33b4727aee8797e0597f31f5
Steve Haroz <steveharoz@users.noreply.github.com>
2023-12-18 15:39:25 +0100

    fix issues from php upgrade (close #85)

--- readcsv.php
+++ readcsv.php
@@ -1,7 +1,7 @@
 <?php
 $csv = array_map('str_getcsv', file($dataFilename));
 array_walk($csv, function(&$a) use ($csv) {
-  $a = array_combine($csv[0], $a);
+  $a = array_merge($csv[0], $a);
 });
 array_shift($csv); # remove column header
 
